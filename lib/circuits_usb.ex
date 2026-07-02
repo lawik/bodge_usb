f6223a8efdc616813d63c3401cfe3102a04dc395
Lars Wikman <lars@underjord.io>
2026-07-02 11:40:45 +0200

    🔥 initial commit 🔥

---
+++ lib/circuits_usb.ex
@@ -0,0 +1,18 @@
+defmodule CircuitsUsb do
+  @moduledoc """
+  Documentation for `CircuitsUsb`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CircuitsUsb.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/circuits_usb_test.exs
@@ -0,0 +1,8 @@
+defmodule CircuitsUsbTest do
+  use ExUnit.Case
+  doctest CircuitsUsb
+
+  test "greets the world" do
+    assert CircuitsUsb.hello() == :world
+  end
+end
